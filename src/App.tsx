84742f8442df06cb68ab3af4474409a55b279708
KimJoonSeo <junjun901225@gmail.com>
2023-09-18 21:18:02 +0900

    Design: "추가 Layout"
    - Layout 도입으로 header, content, footer 구조로 나눔
    - UI 컴포넌트를 따로 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {EspnScoreInterface} from './types';
-import axios from "axios";
-import {QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
-import {ScoreCard} from "./components/ScoreCard";
+import {Col, DatePicker, DatePickerProps, Layout, Row, Space, Spin} from "antd";
+import dayjs from "dayjs";
+import {DashBoard} from "./components/ScoreCard";
 
-function useScoreBoard(date: string, league: string) {
-    const fetchScoreBoard = async (): Promise<EspnScoreInterface> => await axios
-            .get(`https://site.api.espn.com/apis/site/v2/sports/soccer/${league}/scoreboard?dates=${date}&calendartype=blacklist`)
-            .then(res => res.data);
 
-    return useQuery([`${date}-${league}`], fetchScoreBoard);
-}
+
+
 
 function App() {
-    const { status, data,
-        error, isFetching, isSuccess } = useScoreBoard('20230916', 'eng.1');
+    const [date, setDate] = useState<dayjs.Dayjs>(dayjs());
+    const [league, setLeague] = useState<string>('eng.1');
+
+    const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+        if(date) {
+            setDate(date);
+        }
+    }
     return (
-        <div>
-            <h1>Posts</h1>
-            <div>
-                { status === "loading" && "Loading..."
-                }
-                { error instanceof Error &&
-                    <span>Error: {error.message}</span>
-                }
-                { isSuccess && (
-                    <>
-                        {data?.events.map((event, i) => (
-                            <ScoreCard key={i} home={event.competitions[0].competitors[0]} away={event.competitions[0].competitors[1]} status={event.status} startDate={event.competitions[0].startDate}/>
-                        ))
-                        }
-                        <div>{isFetching ? "Background Updating..." : " "}</div>
-                    </>
-                )}
-            </div>
-        </div>
+            <Layout>
+                <Layout.Header>header</Layout.Header>
+                <Layout>
+                    <Layout.Content>
+                        <DatePicker onChange={onChange} defaultValue={date} />
+                        <DashBoard date={date.format('YYYYMMDD').toString()} league={league} />
+                    </Layout.Content>
+                </Layout>
+                <Layout.Header>footer</Layout.Header>
+            </Layout>
     );
 }
 
--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -1,18 +1,20 @@
-import {Avatar, Card, Typography} from "antd";
-import {Competitor, Status} from "../types";
+import {Avatar, Card, Col, Row, Spin, Typography} from "antd";
+import {Competitor, EspnScoreInterface, Status} from "../types";
+import React from "react";
+import axios from "axios";
+import {useQuery} from "@tanstack/react-query";
 
 interface ScoreCardProps {
     home: Competitor;
     away: Competitor;
     status: Status;
-    startDate: string;
 }
 
 function Team({team, score, records}: Competitor) {
     return <Card.Meta
         avatar={<Avatar src={team.logo}
                         style={{backgroundColor: `#${team.color}`, verticalAlign: 'middle'}}
-                        shape="square"/>}
+                />}
         title={
             <div className="ant-card-head-wrapper">
                 <div className="ant-card-head-title">{team.abbreviation}</div>
@@ -23,12 +25,12 @@ function Team({team, score, records}: Competitor) {
         description={`(${records[0].summary})`}
     />;
 };
-export function ScoreCard({home, away, status, startDate}: ScoreCardProps) {
+export function ScoreCard({home, away, status}: ScoreCardProps) {
     let title = 'unknown';
     let style = { color: '' };
     switch(status.type.state) {
         case 'pre':
-            title = startDate;
+            title = status.type.detail.split('at ')[1];
             style.color = '#b9b7b7';
             break;
         case "in":
@@ -47,4 +49,38 @@ export function ScoreCard({home, away, status, startDate}: ScoreCardProps) {
             <Team team={away.team} score={away.score} records={away.records} />
         </Card>
     );
+}
+
+function useScoreBoard(date: string, league: string) {
+    const fetchScoreBoard = async (): Promise<EspnScoreInterface> => await axios
+        .get(`https://site.api.espn.com/apis/site/v2/sports/soccer/${league}/scoreboard?dates=${date}&calendartype=blacklist`)
+        .then(res => res.data);
+
+    return useQuery([date, league], fetchScoreBoard);
+}
+
+export function DashBoard({date, league}: {date: string, league: string}) {
+    const { status, data,
+        error, isFetching, isSuccess } = useScoreBoard(date, league);
+    return (
+        <>
+            { status === "loading" && <Spin />}
+            { error instanceof Error &&
+                <span>Error: {error.message}</span>
+            }
+            { isSuccess && (
+                <Row justify="space-between">
+                    {data?.events.map((event, i) => (
+                        <Col span={12} key={i}>
+                            <ScoreCard home={event.competitions[0].competitors[0]}
+                                       away={event.competitions[0].competitors[1]}
+                                       status={event.status} />
+                        </Col>
+                    ))
+                    }
+                    <div>{isFetching ? "Background Updating..." : " "}</div>
+                </Row>
+            )}
+        </>
+    )
 }
