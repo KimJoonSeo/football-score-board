8149bdaf033b25a940b26c7ea1e6569e0c8f0af2
KimJoonSeo <junjun901225@gmail.com>
2023-09-22 22:37:21 +0900

    Feature: "디버깅을 위한 react query devtools 적용."

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +13,7 @@ const queryClient = new QueryClient();
 root.render(
     <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
 );
 
