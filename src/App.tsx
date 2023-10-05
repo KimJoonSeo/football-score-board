9c3154b375e267b749a3b40f6d2c6336e1364be7
KimJoonSeo <junjun901225@gmail.com>
2023-10-05 22:12:21 +0900

    Fix: replace country flags to league images

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Col, DatePicker, DatePickerProps, Layout, Row, Select} from "antd";
+import {Avatar, Col, DatePicker, DatePickerProps, Layout, Row, Select} from "antd";
 import dayjs from "dayjs";
 import {DashBoard} from "./components";
+import ucl from "./resources/ucl.png";
+import bundesliga from "./resources/bundesliga.png";
+import laliga from "./resources/laliga.png";
+import epl from "./resources/epl.png";
+
+const LeagueOption: React.FC<{image: string, name: string}> = ({image, name}) => (
+    <>
+        <Avatar shape={"square"} src={<img src={image} alt={name}/>} size={"small"}/> {name}
+    </>
+)
 
 const App: React.FC = () => {
     const [date, setDate] = useState<dayjs.Dayjs>(dayjs());
     const [league, setLeague] = useState<string>('eng.1');
     const leagueOptions = [
-        { value: 'eng.1', label: '🏴󠁧󠁢󠁥󠁮󠁧󠁿 Premier League' },
-        { value: 'esp.1', label: '🇪🇸 LaLiga' },
-        { value: 'ger.1', label: '🇩🇪 Bundesliga' },
-        { value: 'uefa.champions', label: '🌍 UEFA Champions League' },
+        { value: 'eng.1', label: <LeagueOption image={epl} name={'Premier League'} /> },
+        { value: 'esp.1', label: <LeagueOption image={laliga} name={'LaLiga'} /> },
+        { value: 'ger.1', label: <LeagueOption image={bundesliga} name={'Bundesliga'} /> },
+        { value: 'uefa.champions', label: <LeagueOption image={ucl} name={'UEFA Champions League'} />},
     ];
 
     const onChangeDate: DatePickerProps['onChange'] = (date, _dateString) => {
