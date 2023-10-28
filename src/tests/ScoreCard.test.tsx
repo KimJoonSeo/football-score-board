e9a946ef3f774dd3cf5827424d1f087e23bb0a2a
KimJoonSeo <junjun901225@gmail.com>
2023-10-28 23:51:27 +0900

    fix: refactor a test code file

--- src/tests/ScoreCard.test.tsx
+++ src/tests/ScoreCard.test.tsx
@@ -1,14 +1,20 @@
-import {mockData, renderWithClient} from "./utils";
-import {DashBoard, ScoreCard} from "../components";
-import {render, screen} from "@testing-library/react";
+import { mockData } from './utils'
+import { ScoreCard } from '../components'
+import { render, screen } from '@testing-library/react'
 
 describe('ScoreCard', () => {
-    test('renders a record of team', () => {
-        // renderWithClient(<DashBoard date={'20231021'} league={'eng.1'} />)
-        // const loadingElement = screen.getByText(/Loading/)
-        // expect(loadingElement).toBeInTheDocument()
-        render(<ScoreCard status={mockData.events[0].status} competitions={mockData.events[0].competitions} />)
-        const recordElement = screen.getByText('('+mockData.events[0].competitions[0].competitors[0].records[0].summary+')')
-        expect(recordElement).toBeInTheDocument()
-    })
+  test('renders a record of team', () => {
+    render(
+      <ScoreCard
+        status={mockData.events[0].status}
+        competitions={mockData.events[0].competitions}
+      />,
+    )
+    const recordElement = screen.getByText(
+      '(' +
+        mockData.events[0].competitions[0].competitors[0].records[0].summary +
+        ')',
+    )
+    expect(recordElement).toBeInTheDocument()
+  })
 })
