ae120bbd7c07c3040d14ca3a9dce71a99cf1f09f
KimJoonSeo <junjun901225@gmail.com>
2023-12-04 22:45:46 +0900

    feat: add a context to manage pagination

---
+++ src/contexts/index.tsx
@@ -0,0 +1,28 @@
+import {createContext, useState} from "react";
+
+const setCurrentPage: any = () => {}
+const setTotalCount: any = () => {}
+const PaginationContext = createContext({
+    state: { currentPage: 1, totalCount: 1},
+    actions: {
+        setCurrentPage: setCurrentPage,
+        setTotalCount: setTotalCount,
+    }
+})
+
+const PaginationProvider = ({ children }: {children: any}) => {
+    const [currentPage, setCurrentPage] = useState(1)
+    const [totalCount, setTotalCount] = useState(1)
+
+    const value = {
+        state: { currentPage, totalCount },
+        actions: { setCurrentPage, setTotalCount },
+    }
+
+    return (
+        <PaginationContext.Provider value={value}>{children}</PaginationContext.Provider>
+    )
+
+}
+
+export { PaginationContext, PaginationProvider }
